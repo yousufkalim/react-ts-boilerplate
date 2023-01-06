d005bc5fbb828213255f3e12274bdce43e86e11e
Yousuf Kalim <yousufkalim@outlook.com>
2023-01-06 18:27:44 +0500

    Added react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
 import Router from 'react-easy-router';
+import { ToastContainer } from 'react-toastify';
 
 // Routes
 import routes from './routes';
 
 // CSS Integration
+import 'react-toastify/dist/ReactToastify.css';
 import './css/index.css';
 
 const App: FC = () => {
@@ -12,7 +14,13 @@ const App: FC = () => {
     return localStorage.getItem('token') !== null;
   };
 
-  return <Router routes={routes} isAuthenticated={checkAuth} />;
+  return (
+    <div className="App">
+      <ToastContainer position="bottom-right" autoClose={3000} />
+
+      <Router routes={routes} isAuthenticated={checkAuth} />
+    </div>
+  );
 };
 
 export default App;
